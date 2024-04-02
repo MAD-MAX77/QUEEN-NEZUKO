3061639e37333f7020c8b094867ac26182dbd2af
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-04-02 08:24:13 +0530

    𝗤𝗨𝗘𝗘𝗡 𝗡𝗘𝗭𝗨𝗞𝗢

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ module.exports = {
     const { plugin, myfunc, stylish } = require('../lib');
     const Ping = startPing - performance.now();
     const caption = `\`\`\`
-❖ ── ✦ ──『✙ Alive ✙』── ✦ ── ❖
+`
 
 ‎ ‎ ‎ ‎ ‎ ‎ ‎ 𝗡𝗝𝗔𝗡 𝗖𝗛𝗔𝗧𝗛𝗜𝗧𝗧𝗜𝗟𝗔 𝗠𝗪𝗢𝗡𝗨𝗨💀
 ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎
@@ -19,22 +19,18 @@ module.exports = {
 ‎ ‎ ‎ ‎ ‎ ‎ 𝗜𝗡𝗜 𝗔𝗟𝗜𝗩𝗘 𝗔𝗗𝗜𝗖𝗛𝗔   ‎𝗧𝗛𝗔𝗧𝗧𝗜𝗞𝗔𝗟𝗔𝗬𝗨𝗠 ‎ ‎ ‎ ‎ ‎‎𝗣𝗨𝗡𝗡𝗔𝗥𝗔 𝗠𝗪𝗢𝗡𝗘🫵☠️
 
 
-*R𝚎𝚙𝚕𝚢 A N𝚞𝚖𝚋𝚎𝚛 T𝚘 G𝚎𝚝:*
-   𝟭 𝗔𝗹𝗹𝗺𝗲𝗻𝘂
-   𝟮 𝗟𝗶𝘀𝘁𝗺𝗲𝗻𝘂
-_ID: QA01_
 `;
 
-  const ownerq = { key: { participant: '0@s.whatsapp.net', ...(pika.chat ? { remoteJid: 'status@broadcast' } : {}), }, message: { contactMessage: { displayName: Config.ownername, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${stylish.fancy31(Config.ownername)},;;;\nFN:${stylish.fancy31(Config.ownername)}\nitem1.TEL;waid=${Config.ownernumber}:${Config.ownernumber}\nitem1.X-ABLabel:Mobile\nEND:VCARD`, jpegThumbnail: Config.image_3, thumbnail: Config.image_3, sendEphemeral: true, }, }, };
-    await anyaV2.sendMessage(pika.chat, {
-      video: Config.IMG-20240329-WA0011.jpg,
+  const ownerq = { key: { participant: '0@s.whatsapp.net', ...(zen.chat ? { remoteJid: 'status@broadcast' } : {}), }, message: { contactMessage: { displayName: Config.ownername, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${stylish.fancy31(Config.ownername)},;;;\nFN:${stylish.fancy31(Config.ownername)}\nitem1.TEL;waid=${Config.ownernumber}:${Config.ownernumber}\nitem1.X-ABLabel:Mobile\nEND:VCARD`, jpegThumbnail: https://i.imgur.com/452xilv.jpeg, thumbnail: https://i.imgur.com/452xilv.jpeg, sendEphemeral: true, }, }, };
+    await queennezuko.sendMessage(zen.chat, {
+      video: https://i.imgur.com/xZ45hxR.jpeg,
       caption: caption,
       gifPlayback: true,
       contextInfo: {
         externalAdReply: {
           title: Config.botname,
           body: '𝗜𝗠 𝗔𝗟𝗜𝗩𝗘🌀',
-          thumbnail: Config.IMG-20240329-WA0011.jpg,
+          thumbnail: https://i.imgur.com/452xilv.jpeg,
           showAdAttribution: true
         }
       }
